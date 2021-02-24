32b4a8a9d04ab951052a743973138f6623c9d425
Karl <karltimmins@gmail.com>
2021-02-24 17:57:33 +0000

    addition of views.py function for detail view form

--- wallet/urls.py
+++ wallet/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.wallet, name='wallet')
+    path('', views.wallet, name='wallet'),
+    path('add/<item_id>', views.add_to_wallet, name='add_to_wallet'),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -7,3 +7,25 @@ def wallet(request):
     """A view to return to sale page"""
 
     return render(request, 'wallet.html')
+
+
+def add_to_wallet(request, item_id):
+    """add property to wallet"""
+
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    date = request.POST.get('date')
+    email = request.POST.get('email')
+    duration = request.POST.get('duration')
+
+    # redirect_url = request.POST.get('redrect_url')
+    wallet = request.session.get('wallet', {})
+
+    wallet[item_id] = [first_name, last_name, date, email, duration]
+    # wallet[item_id] = last_name
+    # wallet[item_id] = email
+    # wallet[item_id] = duration
+
+    request.session['wallet'] = wallet
+    print(request.session['wallet'])
+    return render(request, 'wallet.html')
