d6bb28ab116fd758fc6dcc8914de64ea25e4f92e
Karl <karltimmins@gmail.com>
2021-02-24 23:24:37 +0000

    added allowed host + disabled collect static on heroku

--- full_stack/settings.py
+++ full_stack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8yifj3rhj1phm401(suaf(5lysxncr&_l0gnh3-*o7kwbmbj88'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kerrymount-estates.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,13 +107,17 @@ WSGI_APPLICATION = 'full_stack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
