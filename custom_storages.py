f4cedc279fb4e664c0c5f871daac7e0a63dfd672
Karl <karltimmins@gmail.com>
2021-02-25 00:42:05 +0000

    added aws static + media files

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3boto3Storage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
 
