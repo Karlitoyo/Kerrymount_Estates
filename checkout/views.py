2216107d412fb18292127fe1d022e85558a04ef4
Karl <karltimmins@gmail.com>
2021-02-25 17:41:54 +0000

    Addition of stripe payments to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_5187i0yKX6mxq0Mg3EEWMwEpkMMCLJQFbaGKWR3s7jOTcHazAas1WC48VR2R7E52wVmMcNToexm2msbiiHMSxiRZ900w5fnxd6D',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
