caf3a65e8763c38e3b4589d8942f55f28b0b9010
Karl <karltimmins@gmail.com>
2021-02-25 23:59:22 +0000

    amended property detail view

--- wallet/views.py
+++ wallet/views.py
@@ -12,20 +12,26 @@ def wallet(request):
 def add_to_wallet(request, item_id):
     """add property to wallet"""
 
-    first_name = request.POST.get('first_name')
-    last_name = request.POST.get('last_name')
-    date = request.POST.get('date')
-    email = request.POST.get('email')
-    duration = request.POST.get('duration')
+    quantity = int(request.POST.get('quantity'))
+    # last_name = request.POST.get('last_name')
+    # date = request.POST.get('date')
+    # email = request.POST.get('email')
+    # duration = request.POST.get('duration')
 
     # redirect_url = request.POST.get('redrect_url')
     wallet = request.session.get('wallet', {})
 
+    # if item_id in list(wallet.keys()):
+
     if item_id in list(wallet.keys()):
-        wallet[item_id] = [first_name, last_name, date, email, duration]
+        wallet[item_id] += quantity
+    else:
+        wallet[item_id] = quantity
+
     # wallet[item_id] = last_name
     # wallet[item_id] = email
     # wallet[item_id] = duration
+
     request.session['wallet'] = wallet
     print(request.session['wallet'])
     return render(request, 'wallet.html')
