1ccddaad82ea18d0c36639fa686710d9eba81518
Karl <karltimmins@gmail.com>
2021-02-26 16:07:32 +0000

    amended search function and re-uploaded pictures to admin on django

--- checkout/models.py
+++ checkout/models.py
@@ -49,8 +49,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.price
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.sku}'
