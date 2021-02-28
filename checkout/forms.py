97bdcc4eeb0b7fb5ce1c5038d3c7507651ebcb82
Karl <karltimmins@gmail.com>
2021-02-28 21:33:56 +0000

    Amended propertys.html to fix bug allowing users to progress to checkout without being signed in with if statement on product container

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,3 +6,26 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'full_address', 'phone', 'email')
+
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Surname',
+            'full_address': 'Full Address',
+            'email': 'Email Address',
+            'phone': 'Phone Number',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
